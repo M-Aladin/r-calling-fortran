71f00ad3c9fdf79d487899cce68bf20773e81f66
superuser mintkde <mintkde@reyesaguayo.com>
2017-03-15 16:18:19 -0500

    Merge branch 'feature/arr-20170315-testing_in_linux' into develop
    fix cube to return a vector of cubes

--- cube.f
+++ cube.f
@@ -1,12 +1,13 @@
       subroutine cube(n, x)
 
       integer n
-      double precision x(n)
+      double precision x
+      dimension x(n)
       integer i
 
       do 100 i = 1, n
-        x(i) = x(i) ** 3
+        x(i) = i ** 3
+        !write(*,*) x(i)
  100  continue
-
       end
 
